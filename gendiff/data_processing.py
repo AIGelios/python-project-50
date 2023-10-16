48837e8b75b421709a4a7648c7218341b1cf9b32
ai_gelios <ai_gelios@mail.ru>
2023-10-16 12:45:45 +0500

    refactoring in genndiff.data_processing.py - fix complexity

--- gendiff/data_processing.py
+++ gendiff/data_processing.py
@@ -1,18 +1,25 @@
-def dict_diff(old_dict, new_dict):
-    def is_dict(data):
-        return isinstance(data, dict)
+def is_dict(data):
+    return isinstance(data, dict)
 
+
+def check_values(value1, value2):
+    if is_dict(value1) and is_dict(value2):
+        return (dict_diff(value1, value2), dict_diff(value1, value2))
+    elif is_dict(value1):
+        return (dict_diff(value1), value2)
+    elif is_dict(value2):
+        return (value1, dict_diff(value2))
+    else:
+        return value1, value2
+
+
+def dict_diff(old_dict, new_dict={}):
+    new_dict = new_dict if new_dict else old_dict
     result = []
     for key in sorted(old_dict | new_dict):
         old_value = old_dict.get(key, {None})
         new_value = new_dict.get(key, {None})
-        if is_dict(old_value) and is_dict(new_value):
-            old_value = new_value = dict_diff(old_value, new_value)
-        else:
-            if is_dict(old_value):
-                old_value = dict_diff(old_value, old_value)
-            if is_dict(new_value):
-                new_value = dict_diff(new_value, new_value)
+        old_value, new_value = check_values(old_value, new_value)
         entry = {'key': key,
                  'old_value': old_value,
                  'new_value': new_value,
