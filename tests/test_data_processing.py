1a0bfda70da17d0584fd546da3678b63e514e95c
ai_gelios <ai_gelios@mail.ru>
2023-10-20 15:17:51 +0500

    fix indents and brackets in tests/test_data_processing.py

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -8,25 +8,27 @@ flat_diff = [
     {'key': 'aa', 'old_value': 1, 'new_value': NO_ITEM},
     {'key': 'b', 'old_value': True, 'new_value': False},
     {'key': 'c', 'old_value': NO_ITEM, 'new_value': 'xyz'},
-    {'key': 'd', 'old_value': 1.5, 'new_value': 1.5}
-    ]
+    {'key': 'd', 'old_value': 1.5, 'new_value': 1.5},
+]
 
 nested_dict_1 = {'a': {'b': 1, 'c': True}, 'd': 'no'}
 nested_dict_2 = {'a': {'b': 2, 'c': False}, 'd': 'yes'}
 nested_diff = [
-        {
-            'key': 'a',
-            'old_value': [
-                {'key': 'b', 'old_value': 1, 'new_value': 2},
-                {'key': 'c', 'old_value': True, 'new_value': False}
-                ],
-            'new_value': [
-                {'key': 'b', 'old_value': 1, 'new_value': 2},
-                {'key': 'c', 'old_value': True, 'new_value': False}
-                ]
-        },
-        {'key': 'd', 'old_value': 'no', 'new_value': 'yes'}
-    ]
+    {'key': 'a',
+     'old_value': [
+         {'key': 'b', 'old_value': 1, 'new_value': 2},
+         {'key': 'c', 'old_value': True, 'new_value': False},
+         ],
+     'new_value': [
+         {'key': 'b', 'old_value': 1, 'new_value': 2},
+         {'key': 'c', 'old_value': True, 'new_value': False},
+         ]
+     },
+    {'key': 'd',
+     'old_value': 'no',
+     'new_value': 'yes'
+     }
+]
 
 
 def test_dict_diff():
